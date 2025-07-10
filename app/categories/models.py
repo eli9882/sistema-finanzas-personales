42912e6be7be74d1047a26c675e9973e878615bc
Jose M <josemurillo1620@gmail.com>
2025-07-10 16:02:42 -0600

    correccion de errores Lint

--- app/categories/models.py
+++ app/categories/models.py
@@ -2,10 +2,12 @@
 from django.db import models
 from core.models import User  # importas tu modelo personalizado
 
+
 class Categoria(models.Model):
     nombre = models.CharField(max_length=45)
     descripcion = models.CharField(max_length=150)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categorias')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, 
+    related_name='categorias')
 
     def __str__(self):
         return self.nombre
--- app/events/models.py
+++ app/events/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from core.models import User
 from categories.models import Categoria
 
+
 class EventoFinanciero(models.Model):
     TIPO_CHOICES = [
         ('Ingreso', 'Ingreso'),
@@ -14,11 +15,14 @@ class EventoFinanciero(models.Model):
     monto = models.DecimalField(max_digits=10, decimal_places=2)
     fecha = models.DateField()
     descripcion = models.CharField(max_length=150)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='eventos')
-    categorias = models.ManyToManyField(Categoria, through='CategoriaEvento', related_name='eventos')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, 
+    related_name='eventos')
+    categorias = models.ManyToManyField(Categoria, 
+    through='CategoriaEvento', related_name='eventos')
 
     def __str__(self):
         return f"{self.tipo} - {self.monto} ({self.fecha})"
+
 
 class CategoriaEvento(models.Model):
     evento = models.ForeignKey(EventoFinanciero, on_delete=models.CASCADE)
