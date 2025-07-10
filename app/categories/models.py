6b12c407d09b72dbc2301fd6623bac616ab22e30
Jose M <josemurillo1620@gmail.com>
2025-07-10 16:11:59 -0600

    correccion de errores salto de linea

--- app/categories/models.py
+++ app/categories/models.py
@@ -6,8 +6,11 @@ from core.models import User  # importas tu modelo personalizado
 class Categoria(models.Model):
     nombre = models.CharField(max_length=45)
     descripcion = models.CharField(max_length=150)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, 
-    related_name='categorias')
+    usuario = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='categorias'
+    )
 
     def __str__(self):
         return self.nombre
--- app/events/models.py
+++ app/events/models.py
@@ -15,10 +15,16 @@ class EventoFinanciero(models.Model):
     monto = models.DecimalField(max_digits=10, decimal_places=2)
     fecha = models.DateField()
     descripcion = models.CharField(max_length=150)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, 
-    related_name='eventos')
-    categorias = models.ManyToManyField(Categoria, 
-    through='CategoriaEvento', related_name='eventos')
+    usuario = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='eventos'
+    )
+    categorias = models.ManyToManyField(
+        Categoria,
+        through='CategoriaEvento',
+        related_name='eventos'
+    )
 
     def __str__(self):
         return f"{self.tipo} - {self.monto} ({self.fecha})"
