61a5840b69eccfe0e3cb39473c1e670984d6e11e
Liza988 <140469042+eli9882@users.noreply.github.com>
2025-07-17 23:40:14 -0600

    User Api

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,45 @@
+"""Tests para Django admin modificaciones."""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """Tests para Django admin"""
+
+    def setUp(self):
+        """Crear usuario y cliente."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='1Qaz2wsx',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+    def test_users_list(self):
+        """Test usuarios aparecen en la página."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test página de edición de usuarios."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+    
+    def test_create_user_page(self):
+        """Test página de creación de usuarios."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
