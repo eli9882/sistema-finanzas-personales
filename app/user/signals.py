1be61ca4e6dbc00baf4069153813a2a23da34bce
Liza988 <140469042+eli9882@users.noreply.github.com>
2025-08-01 16:45:03 -0600

    Integracion_Frontend/Backend_Usuario

--- app/user/signals.py
+++ app/user/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 
 
 @receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, **kwargs):
+def password_reset_token_created(sender, reset_password_token, **kwargs):
     email = reset_password_token.user.email
     token = reset_password_token.key
     reset_url = f"http://localhost:5173/reset-password?token={token}"
@@ -19,3 +19,4 @@ def password_reset_token_created(sender, instance, reset_password_token, **kwarg
         recipient_list=[email],
         fail_silently=False,
     )
+
